9f74dc7c360bbded7cbbf56efee43d7d8066250e
igada <iga.dai0226@gmail.com>
2024-06-17 12:04:04 +0900

    practice10-2

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -29,7 +29,7 @@ public class Player extends Character implements KeyListener{
 		}
 		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
 			GameWorld.playerBullets.add(
-					new PlayerBullet(x,y,0,-10));
+					new PlayerBullet(x,y,10,-10));
 			System.out.println(GameWorld.playerBullets.size());
 		}
 	}
