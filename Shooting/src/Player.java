9b16e2faa581b74699553671f95d9813e3be0ac6
igada <iga.dai0226@gmail.com>
2024-06-17 11:19:19 +0900

    10.8コミット

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -4,6 +4,13 @@ public class Player extends Character implements KeyListener{
 	public Player(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
+	
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillRect(x, y+20, 30, 10);
+		f.setColor(200, 200, 200);
+		f.fillRect(x+10, y, 10, 30);
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
