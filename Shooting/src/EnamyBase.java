c718731f70c0acbf9cdf723fc013ee058164b8f0
igada <iga.dai0226@gmail.com>
2024-06-18 10:00:41 +0900

    11.2コミット

--- Shooting/src/EnamyBase.java
+++ Shooting/src/EnamyBase.java
@@ -3,5 +3,10 @@ public class EnamyBase extends Enemy{
 	public EnamyBase(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
+	public void move() {
+		super.move();
+		if(x>300)vx=-1;
+		if(x<100)vx=1;
+	}
 
 }
