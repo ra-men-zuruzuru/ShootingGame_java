40a5a42b026266b151c0e9ec8f7609ec5c894f41
igada <iga.dai0226@gmail.com>
2024-06-18 09:48:16 +0900

    11.1コミット

---
+++ Shooting/src/EnamyBase.java
@@ -0,0 +1,7 @@
+
+public class EnamyBase extends Enemy{
+	public EnamyBase(double x,double y,double vx,double vy) {
+		super(x,y,vx,vy);
+	}
+
+}
--- Shooting/src/PlayerBullet.java
+++ Shooting/src/PlayerBullet.java
@@ -3,5 +3,10 @@ public class PlayerBullet extends Character{
 	public PlayerBullet(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
+	
+	public void draw(MyFrame f) {
+		f.setColor(200, 0, 0);
+		f.fillRect(x+10,y-40,10,40);
+	}
 
 }
