ff537ea4938aceb89031f9edde6e6c54566068fa
igada <iga.dai0226@gmail.com>
2024-06-18 11:19:56 +0900

    practice11-2

--- Shooting/src/RandomEnemy.java
+++ Shooting/src/RandomEnemy.java
@@ -4,6 +4,13 @@ public class RandomEnemy extends Enemy{
 		super(x,y,vx,vy);
 	}
 	
+	public void draw(MyFrame f) {
+		f.setColor(0, 130, 0);
+		f.fillRect(x,y,10,20);
+		f.fillRect(x+20, y, 10, 20);
+		f.fillRect(x+10, y+20, 10, 10);
+	}
+	
 	public void move() {
 		super.move();
 		vx=Math.random()*4-2;
