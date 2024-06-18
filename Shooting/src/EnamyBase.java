01aa55804bf0828eaf34b3dc97a7b9d4bdcd4e89
igada <iga.dai0226@gmail.com>
2024-06-18 10:43:46 +0900

    11.4コミット

--- Shooting/src/EnamyBase.java
+++ Shooting/src/EnamyBase.java
@@ -3,10 +3,20 @@ public class EnamyBase extends Enemy{
 	public EnamyBase(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillOval(x, y, 32, 32);
+		f.setColor(200, 200, 200);
+		f.fillOval(x-16,y+8,64,16);
+	}
+	
 	public void move() {
-		super.move();//キャラクターから
+		super.move();
 		if(x>300)vx=-1;
 		if(x<100)vx=1;
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
+		}
 	}
 
 }
